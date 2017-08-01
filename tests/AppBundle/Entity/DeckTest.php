ae530567dab509179a9d296c4ac06a9f2a96de2e
JasonForsey <jason.forsey@gmail.com>
2017-08-01 23:10:48 +0100

    Added test for a new Deck of 52 cards

---
+++ tests/AppBundle/Entity/DeckTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\AppBundle\Entity;
+
+use AppBundle\Entity\Deck;
+use PHPUnit\Framework\TestCase;
+
+class DeckTest extends TestCase
+{
+    public function testNewDeckCount()
+    {
+        $deck = new Deck();
+
+        $this->assertCount(52, $deck->allCards);
+
+    }
+
+}
