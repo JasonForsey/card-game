17a152e0e3ce3bcb74c4f6ad0e54c9502b721f16
JasonForsey <jason.forsey@gmail.com>
2017-08-02 01:46:57 +0100

    Added the Hand entity that will instatiate a new empty hand as well as allow a Card to be added to a Hand

--- src/AppBundle/Entity/Hand.php
+++ src/AppBundle/Entity/Hand.php
@@ -2,79 +2,28 @@
 
 namespace AppBundle\Entity;
 
-class Card
+class Hand
 {
 
     /**
-     * Suits
+     * @var array $cards
      */
-    const SUIT_HEARTS   = 1;
-    const SUIT_CLUBS    = 2;
-    const SUIT_SPADES   = 3;
-    const SUIT_DIAMONDS = 4;
+    protected $cards = [];
 
     /**
-     * Picture Cards
+     * @param Card $card
      */
-    const TYPE_ACE      = 1;
-    const TYPE_JACK     = 11;
-    const TYPE_QUEEN    = 12;
-    const TYPE_KING     = 13;
-
-    /**
-     * @var array $suits
-     */
-    public $suits = array(
-        self::SUIT_HEARTS   => 'hearts',
-        self::SUIT_DIAMONDS => 'diamonds',
-        self::SUIT_CLUBS    => 'clubs',
-        self::SUIT_SPADES   => 'spades'
-    );
-
-    /**
-     * @var int $suit
-     */
-    protected $suit;
-
-    /**
-     * @var int $value
-     */
-    protected $value;
-
-    /**
-     * Card constructor.
-     * @param int $suit
-     * @param int $value
-     * @throws \InvalidArgumentException
-     */
-    public function __construct($suit, $value)
+    public function addCard(Card $card)
     {
-        // Throw exception with relevant message if parameters are invalid.
-        if (!array_key_exists($suit, $this->suits)){
-            throw new \InvalidArgumentException('Invalid suit was supplied');
-        }
-        if (1 > $value || 13 < $value){
-            throw new \InvalidArgumentException('Invalid value was supplied');
-        }
-
-        $this->suit = $suit;
-        $this->value = $value;
+        $this->cards[] = $card;
     }
 
     /**
      * @return int
      */
-    public function getSuit() : int
+    public function getCardCount() : int
     {
-        return $this->suit;
-    }
-
-    /**
-     * @return int
-     */
-    public function getValue(): int
-    {
-        return $this->value;
+        return count($this->cards);
     }
 
 }
