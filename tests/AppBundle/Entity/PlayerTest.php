2556ab7a66e78363f24ff18e50f3a8d055b48955
JasonForsey <jason.forsey@gmail.com>
2017-08-02 01:59:36 +0100

    New Player test to ensure that a player can be created with a null hand and be able to set a new Hand

---
+++ tests/AppBundle/Entity/PlayerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\AppBundle\Entity;
+
+use AppBundle\Entity\Hand;
+use AppBundle\Entity\Player;
+use PHPUnit\Framework\TestCase;
+
+class PlayerTest extends TestCase
+{
+    public function testNewPlayer()
+    {
+        $player = new Player('testing');
+        $this->assertEquals('testing', $player->getName());
+
+    }
+
+    public function testNoInitialHand()
+    {
+        $player = new Player('testing');
+
+        $this->assertNull($player->getHand);
+
+    }
+
+    public function testGetHand()
+    {
+        $player = new Player('testing');
+        $hand = new Hand();
+
+        $player->newHand($hand);
+
+        $this->assertInstanceOf('AppBundle\Entity\Hand', $player->getHand);
+
+    }
+
+}
