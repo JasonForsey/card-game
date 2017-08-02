e171b27b6104c5bc7885d045e64ccc2c8c4425cb
JasonForsey <jason.forsey@gmail.com>
2017-08-02 03:23:56 +0100

    Added command that will run the game and initially output the number of cards a player has in their hand at the end of the game

---
+++ src/AppBundle/Command/RunGameCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RunGameCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('app:run_game_command')
+            ->setDescription('Hello PhpStorm');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+    }
+}
